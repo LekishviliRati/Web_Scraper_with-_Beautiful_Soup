9dc3185da9b3f19bbdf22ffe91002dfd4b148dd2
LekishviliRati <lekishvili.rati@gmail.com>
2022-07-09 09:29:28 +0200

    Links and links title are scraped from home page and saved in a csv file

--- scraper.py
+++ scraper.py
@@ -1,12 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get("https://koesio-project.herokuapp.com/").text
 
 soup = BeautifulSoup(source, 'lxml')
 
+csv_file = open('scraper.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['title', 'link'])
+
 article = soup.find('article')
 
-headline = article.h1.text
+# print(article.prettify())
 
-print(headline)
+# # Get H1 title from home page
+# headline = article.h1.text
+#
+# print(headline)
+
+# Get links with title from home page save them in a CSV file
+for link in article.find_all('a'):
+    link_href = link.get('href')
+    # print(href_link)
+    link_title = link.text
+    csv_writer.writerow([link_title, link_href])
+
+
+csv_file.close()
